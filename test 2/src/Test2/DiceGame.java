fd03e3cb3aba12449c8cf8db4597787846b8a6f6
TommyBui <tommydbui@yahoo.com>
2013-04-28 23:56:39 -0700

    Update DiceGame.java

--- test 2/src/Test2/DiceGame.java
+++ test 2/src/Test2/DiceGame.java
@@ -6,93 +6,86 @@ public class DiceGame
 
 	public static void main(String[] args) 
 	{
-		//set up  variable 
-				boolean gameNotOver = true;
+	
+		boolean gameNotOver = true;
 
-				//begin do loop
-				do{	
-					//Play the game with 1 die
-					JOptionPane.showMessageDialog( null, "Let play the dice game!" );
-					JOptionPane.showMessageDialog( null, "1-3 is Low and 4-6 is High" );
-
-					
-					Dice game1 = new Dice();
-					int rollResult = game1.Roll();
+			
+		do{	
+			//Play the game with 1 die
+			JOptionPane.showMessageDialog( null, "Let play the dice game!" );
+			JOptionPane.showMessageDialog( null, "1-3 is Low and 4-6 is High" );
 
 					
+			Dice game1 = new Dice();
+			int rollResult = game1.Roll();
 
-					//ask the user to enter high or low
-					String guess = JOptionPane.showInputDialog( null, "Enter (L) for Low or (H) for High" );
+			//ask the user to enter high or low
+			String guess = JOptionPane.showInputDialog( null, "Enter (L) for Low or (H) for High" );
 
 					
 					
-					if (guess.equalsIgnoreCase("H") && (rollResult >= 4))	
-					{
-						JOptionPane.showMessageDialog( null, "Your guess is right! The dice rolled was " + rollResult + "." + " It's High!" );
-					}
-					else if ( (guess.equalsIgnoreCase("L")) && (rollResult <= 3) )
-					{
-						JOptionPane.showMessageDialog( null, "Your guess is right! The dice rolled was " + rollResult + "." + " It's Low!" );			
-					}
-					else if (guess.equalsIgnoreCase("H") && rollResult <= 3)
-					{
-						JOptionPane.showMessageDialog( null, "Your guess is wrong!. The dice rolled was " + rollResult + "." + " It's Low!");
-					}
+			if (guess.equalsIgnoreCase("H") && (rollResult >= 4))	
+			{
+				JOptionPane.showMessageDialog( null, "Your guess is right! The dice rolled was " + rollResult + "." + " It's High!" );
+			}
+			else if ( (guess.equalsIgnoreCase("L")) && (rollResult <= 3) )
+			{
+				JOptionPane.showMessageDialog( null, "Your guess is right! The dice rolled was " + rollResult + "." + " It's Low!" );			
+			}
+			else if (guess.equalsIgnoreCase("H") && rollResult <= 3)
+			{
+				JOptionPane.showMessageDialog( null, "Your guess is wrong!. The dice rolled was " + rollResult + "." + " It's Low!");
+			}
 					
-					else
-					{
-						JOptionPane.showMessageDialog( null, "Your guess is wrong!. The dice rolled was " + rollResult + "." + " It's High!" );
-					}
+			else
+			{
+				JOptionPane.showMessageDialog( null, "Your guess is wrong!. The dice rolled was " + rollResult + "." + " It's High!" );
+			}
 
 
-					//Play the game with 2 dices
-					JOptionPane.showMessageDialog( null, "Ready to play with 2 dice now?" );
-					JOptionPane.showMessageDialog( null, "1-6 is Low and 7-12 is High." );
+			//Play the game with 2 dices
+			JOptionPane.showMessageDialog( null, "Ready to play with 2 dice now?" );
+			JOptionPane.showMessageDialog( null, "1-6 is Low and 7-12 is High." );
 
 				
-					Dice game2 = new Dice();
+			Dice game2 = new Dice();
 
-					int rollResult1 = game1.Roll();
-					int rollResult2 = game2.Roll();
+			int rollResult1 = game1.Roll();
+			int rollResult2 = game2.Roll();
 
-					//total roll
-					int totalRoll = (rollResult1 + rollResult2);
+			//total roll
+			int totalRoll = (rollResult1 + rollResult2);
 
+			//ask the user to enter high or low		
+			guess = JOptionPane.showInputDialog( null, "The dice have been rolled. Guess high (enter h) or low (enter l)." );
 					
+			if ( (guess.equalsIgnoreCase("H")) && (totalRoll >= 7) )
+			{
+				JOptionPane.showMessageDialog( null, "Your guess is righ! The dices rolled was " + totalRoll + "." + " It's High" );
+			}
+			else if ( (guess.equalsIgnoreCase("L")) && (totalRoll <= 6) )
+			{
+				JOptionPane.showMessageDialog( null, "Your guess is righ! The dices rolled was " + totalRoll + "." + " It's Low");			
+			}
+			else if (guess.equalsIgnoreCase("H") && (totalRoll <=6))
+			{
+				JOptionPane.showMessageDialog( null, "Sorry! Your guess is wrong. The dices rolled was " + totalRoll + "." + " It's Low");
+			}
+			else 
+			{
+				JOptionPane.showMessageDialog( null, "Sorry! Your guess is wrong!. The dices rolled was " + totalRoll + "." + " It's High!" );
+			}
 
-					//ask the user to enter high or low		
-					guess = JOptionPane.showInputDialog( null, "The dice have been rolled. Guess high (enter h) or low (enter l)." );
+			String playAgain = JOptionPane.showInputDialog( null, "Would you like to play again? (Y/N)" );
 
-					
-					if ( (guess.equalsIgnoreCase("H")) && (totalRoll >= 7) )
-					{
-					JOptionPane.showMessageDialog( null, "Your guess is righ! The dices rolled was " + totalRoll + "." + " It's High" );
-					}
-					else if ( (guess.equalsIgnoreCase("L")) && (totalRoll <= 6) )
-					{
-						JOptionPane.showMessageDialog( null, "Your guess is righ! The dices rolled was " + totalRoll + "." + " It's Low");			
-					}
-					else if (guess.equalsIgnoreCase("H") && (totalRoll <=6))
-					{
-						JOptionPane.showMessageDialog( null, "Sorry! Your guess is wrong. The dices rolled was " + totalRoll + "." + " It's Low");
-					}
-					else 
-					{
-						JOptionPane.showMessageDialog( null, "Sorry! Your guess is wrong!. The dices rolled was " + totalRoll + "." + " It's High!" );
-					}
+			if (playAgain.equalsIgnoreCase("N"))
+			{
+				gameNotOver = false;
+			}
+		} while(gameNotOver);
 
-					String playAgain = JOptionPane.showInputDialog( null, "Would you like to play again? (Y/N)" );
-
-					if (playAgain.equalsIgnoreCase("N"))
-					{
-					gameNotOver = false;
-					}
-				} while(gameNotOver);
-
-				//exit messages
-				JOptionPane.showMessageDialog( null, "Good bye!" );
-
-
+		//exit messages
+		JOptionPane.showMessageDialog( null, "Good bye!" );
 
 	}
 
